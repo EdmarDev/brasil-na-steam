139192132553a5f9f72e61d7adb34b1b70e7a4be
Edmar Filho <ebgfilho7@gmail.com>
2025-03-07 20:07:00 -0300

    chore: import dotenv for drizzle

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,7 @@
 import {drizzle} from "drizzle-orm/node-postgres"
 import {eq} from "drizzle-orm"
 import * as schema from "./schema"
+import "dotenv/config"
 
 export const drizzleDb = drizzle(process.env.DATABASE_URL!, {
   casing: "snake_case",
