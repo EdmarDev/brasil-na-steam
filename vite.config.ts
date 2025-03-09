ff79a2f950a5cd9f4965edced9b7d10ef9ea7398
Edmar Filho <ebgfilho7@gmail.com>
2025-03-09 16:19:11 -0300

    fix: top section styles on lower resolution

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import {reactRouter} from "@react-router/dev/vite"
 import autoprefixer from "autoprefixer"
 import {defineConfig} from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
+import postcssPresetMantine from "postcss-preset-mantine"
 
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [autoprefixer],
+      plugins: [postcssPresetMantine(), autoprefixer],
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
