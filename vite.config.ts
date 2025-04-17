2019f5a7fe4813ba494e7c5b4c4b0e36edc7082c
Edmar Filho <ebgfilho7@gmail.com>
2025-04-17 12:22:16 -0300

    chore: heroku config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import {reactRouter} from "@react-router/dev/vite"
 import autoprefixer from "autoprefixer"
-import {defineConfig} from "vite"
+import {defineConfig, loadEnv} from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import postcssPresetMantine from "postcss-preset-mantine"
 
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [postcssPresetMantine(), autoprefixer],
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  const PORT = env.PORT || process.env.PORT || "8080"
+
+  return {
+    css: {
+      postcss: {
+        plugins: [postcssPresetMantine(), autoprefixer],
+      },
     },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
+    plugins: [reactRouter(), tsconfigPaths()],
+    server: {
+      port: parseInt(PORT),
+    },
+  }
 })
